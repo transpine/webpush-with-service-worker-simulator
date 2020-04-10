505e8d9253a545acb2d5e3ae51017444f1141f04
JayD <jayd@trineedle.com>
2020-04-10 19:29:56 +0900

    add web-push related parts

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var webpush = require('web-push');
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
@@ -20,6 +21,42 @@ const options = {
     cert: fs.readFileSync('localhost.pem'),
     key: fs.readFileSync('localhost-key.pem')
 };  
+
+const vapidKeys = webpush.generateVAPIDKeys();
+webpush.setVapidDetails(
+    'mailto:transpine@gmail.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
+app.get('/push/key', function(req, res){
+    console.log(`publick key sent: ${vapidKeys.publicKey}`);
+    res.send({
+        key: vapidKeys.publicKey
+    });
+});
+
+const temp_subs = [];
+app.post('/push/subscribe', function(req, res){
+    temp_subs.append(req.body.subscription);
+    console.log(req.body);
+
+    res.send('Success');
+});
+
+app.post('/push/notify', function(req, res){
+    let payload = {};
+    payload.title = req.body.title;
+    payload.message = req.body.message;
+
+    for(const subs in temp_subs){
+        webpush.sendNotification(subs, JSON.stringify(payload))
+        .then(function (response) {
+            console.log('sent notification');
+            res.sendStatus(201);
+        });
+    }
+});
 
 https.createServer(options, app).listen( port, () => {
     console.log(`webpush server running, port:${port}`);
